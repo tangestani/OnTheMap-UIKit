6bbbf147681a476bccdeeb870fbec3ec6df35f8e
Mohammed Tangestani <tangestani@gmail.com>
2020-10-16 15:58:34 +0300

    Initial Commit

---
+++ OnTheMap-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap-UIKit
+//
+//  Created by Mohammed Tangestani on 10/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
